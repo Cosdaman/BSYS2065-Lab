d4c2f586063f9945f97bfcbea97c0fc4f7a6850a
Cosdaman <Cosdaguy@gmail.com>
2018-04-24 15:10:28 -0700

    q4 up

---
+++ Lab4/Lab4Q4.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+def drawsquare(size):
+    for y in range(4):
+        toit.pendown()
+        toit.forward(size)
+        toit.left(90)
+
+
+wn = turtle.Screen()
+toit = turtle.Turtle()
+wn.bgcolor("light green")
+toit.color("blue")
+toit.pensize(3)
+toit.speed(0)
+
+for i in range(24):
+    drawsquare(80)
+    toit.left(15)
+
+wn.exitonclick()
