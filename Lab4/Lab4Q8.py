58ebcc3981df8fc2c1affc9558b807155c7efee0
Cosdaman <Cosdaguy@gmail.com>
2018-04-24 15:26:01 -0700

    q8 up

--- Lab4/Lab4Q8.py
+++ Lab4/Lab4Q8.py
@@ -1,1 +1,10 @@
-#Write a function areaOfCircle(r) which returns the area of a circle of radius r. Make sure you use the math module in your solution.
+# Write a function areaOfCircle(r) which returns the area of a circle of radius r.
+# Make sure you use the math module in your solution.
+
+import math
+
+radius = input("Please input a radius: ")
+radius = int(radius)
+area = math.pi * radius**2
+print("The area is: ", area)
+input("Press enter to continue...")
