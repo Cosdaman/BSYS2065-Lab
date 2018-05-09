55bc8b7c5211bc7101bc08782464c43e151e28b3
Cosdaman <Cosdaguy@gmail.com>
2018-05-09 13:34:51 -0700

    gabe here bitch

---
+++ Lab6/Lab6Q8-2.py
@@ -0,0 +1,20 @@
+# Write a function print_triangular_numbers(n) that prints out the first n triangular numbers.
+# A call to print_triangular_numbers(5) would produce the following output:
+# 1       1
+# 2       3
+# 3       6
+# 4       10
+# 5       15
+
+
+def print_triangular_numbers(n):
+    number = int(0)
+    for i in range(n):
+        number = number + i
+        print(number)
+
+
+x = int(input("How many triangular numbers do you want to see? Enter here: "))
+
+print_triangular_numbers(x)
+
